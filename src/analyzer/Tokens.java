141950e9728c0ad6cf75d348e500679afcc97db4
1170300809 <47967696+1170300809@users.noreply.github.com>
2020-04-03 12:03:13 +0800

    1.4
    增加了一个getTokenListCopy功能，能够获取tokenList的复制品以在GUI中得到他的Token信息

--- src/analyzer/Tokens.java
+++ src/analyzer/Tokens.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 public class Tokens {
 
-  public static final String wordSerialNumberFilePath = "src/analyzer/CodeList";
-
   private List<Token> tokenList = new ArrayList<>();
   BufferedReader br;
   private HashMap<String, Integer> type_SerialNum;
@@ -31,31 +29,37 @@ public class Tokens {
     this.tokenList.add(new Token(wordSerialNumber, wordValue));
   }
 
-  public void add(Token newToken) {
-    this.tokenList.add(newToken);
-  }
-
   /**
-   * Each line of the format file is read in a "type coded" style and
-   * loaded into the Map
+   * Each line of the format file is read in a "type coded" style and loaded into the Map
    * 
    * @param filePath
    */
   private void loadWordSerialNumberFile(String filePath) {
     try {
-      this.br = new BufferedReader(new FileReader(new File(filePath)));
-      this.type_SerialNum = new HashMap<>();
+      br = new BufferedReader(new FileReader(new File(filePath)));
+      type_SerialNum = new HashMap<>();
       String line;
-      while ((line = this.br.readLine()) != null) {
+      while ((line = br.readLine()) != null) {
         String[] array_type_num = line.split(" ");
         String type = array_type_num[0];
         int num = Integer.parseInt(array_type_num[1]);
-        this.type_SerialNum.put(type, num);
+        type_SerialNum.put(type, num);
       }
     } catch (Exception e) {
       e.printStackTrace();
     }
 
+
+
+  }
+
+  /**
+   * Gets a copy of the Token list
+   * 
+   * @return a copy of the Token list
+   */
+  public ArrayList<Token> getTokenListCopy() {
+    return new ArrayList<Token>(tokenList);
   }
 
   /**
@@ -72,16 +76,14 @@ public class Tokens {
   }
 
   /**
-   * The type code is generated according to the loaded Map and the
-   * input string
+   * The type code is generated according to the loaded Map and the input string
    * 
    * @param wordType
    * @return type code
    */
-  public int wordTypeToSerialNumber(String wordType) {
-    if (this.type_SerialNum.containsKey(wordType)) {
-      return this.type_SerialNum.get(wordType);
-    }
+  private int wordTypeToSerialNumber(String wordType) {
+    if (type_SerialNum.containsKey(wordType))
+      return type_SerialNum.get(wordType);
     return 0;
   }
 
@@ -89,20 +91,19 @@ public class Tokens {
    * Returns a character type based on the type code
    * 
    * @param wordSerialNumber
-   * @return wordType
+   * @return worTtype
    */
-  public String wordSerialNumberToType(int wordSerialNumber) {
-    for (String s : this.type_SerialNum.keySet()) {
-      if (this.type_SerialNum.get(s) == wordSerialNumber) {
+  private String wordSerialNumberToType(int wordSerialNumber) {
+    for (String s : type_SerialNum.keySet()) {
+      if (type_SerialNum.get(s) == wordSerialNumber)
         return s;
-      }
     }
     return "None";
   }
 
   public static void main(String[] args) {
     /* test */
-    Tokens ts = new Tokens(wordSerialNumberFilePath);
+    Tokens ts = new Tokens("src/analyzer/CodeList");
     System.out.println(ts.wordSerialNumberToType(19));
     System.out.println(ts.wordTypeToSerialNumber("+"));
     Tokens ts1 = ts.getTokensCopy();
